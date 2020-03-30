3e0321b6544e5a6359c1677d5494000874a924b1
oakfap <oakbahina@gmail.com>
2020-03-30 22:54:18 +0700

    Test commit

--- src/online_university/Online_University.java
+++ src/online_university/Online_University.java
@@ -5,10 +5,10 @@
  */
 package online_university;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 /**
  *
@@ -20,11 +20,25 @@ public class Online_University {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       Database db = new Database();
-       ArrayList<String> t = new ArrayList<String>();
-       for(int i = 0; i<10;i++) t.add(""+i);
-       db.write(t);
-       db.read();
+        Database db = new Database();
+        ArrayList<A> t = new ArrayList<A>();
+        for(int i = 0 ; i<10;i++)
+        t.add(new A());
+        db.write(t);
+        db.read();
+        t = (ArrayList<A>)db.get();
+        for(A i : t) System.out.println(i);
     }
+    public static class A implements Serializable{
+        static int count = 0;
 
+        public A() {
+            count++;
+        }
+        @Override
+        public String toString() {
+            return "Class A"+count; //To change body of generated methods, choose Tools | Templates.
+        }
+         
+    }
 }
